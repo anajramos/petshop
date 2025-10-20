9103c336d2b042b677ccacf544c7d08b28e48169
anajramos <ana.j.ramos9@aluno.senai.br>
2025-10-20 11:56:08 -0300

    feat:deleta games

--- src/controllers/games.js
+++
@@ -1,6 +0,0 @@
-export const jogosPreferidos = [
-    { id: 1, nome: "The Legend of Zelda", plataforma: "Nintendo Switch", ano: 2017 },
-    { id: 2, nome: "God of War", plataforma: "PlayStation 5", ano: 2022 },
-    { id: 3, nome: "Minecraft", plataforma: "Multiplataforma", ano: 2011 },
-
-];
